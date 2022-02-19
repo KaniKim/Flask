cb3c91266991ecb71da53353387eed20b92dc3f6
KaniKim <kkh5428@gmail.com>
2022-02-19 19:46:58 +0900

    minor change check

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLACLCEHMY_DATABASE_URL = "ex"
+SQLACLCEHMY_DATABASE_URL = "postgresql"
 
 engine = create_engine(SQLACLCEHMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
